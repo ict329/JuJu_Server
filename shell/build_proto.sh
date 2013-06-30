3f0328c9f7fd78d09748abbf832004c6eff8a545
ict-pc <ict329@ict.com>
2013-07-01 00:08:25 +0800

    add proto files

---
+++ shell/build_proto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DST_DIR=../pbmodel
+SRC_DIR=../proto
+
+
+cd $SRC_DIR
+Files=`ls *.proto`
+
+echo "Start to build proto files for Python"
+
+for file in $Files
+    do 
+        echo "Build file: $file"
+        protoc -I=$SRC_DIR --python_out=$DST_DIR $SRC_DIR/$file
+    done
+
+echo "Done"
