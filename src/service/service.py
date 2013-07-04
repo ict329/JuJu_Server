9fb0991608ba519cda41aa392540b35a22598f28
GamyHuang <hguangm2009@gmail.com>
2013-07-04 14:02:22 +0800

    refactor service

--- src/service/service.py
+++ src/service/service.py
@@ -2,12 +2,22 @@ from flask import request
 from common.utils.strutil import *
 
 class JJService(object):
-    def __init__(self):
+    def __init__(self, request):
         self.code = 0
         self.data = None
+        self.request = request
     
-    def parse_data(self, request):
+    def parse_request(self):
         return True
     
     def handle_data(self):
         return self.__class__.__name__ 
+
+    def check_parameters(self):
+        return True
+
+    def authenticate(self):
+        return True
+
+    def handle(self):
+
